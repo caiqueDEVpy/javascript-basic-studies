efa3778bbeba88e202bd15ad84cf829ba5ac8b6c
Caique Menezes <caiquemenezes1@outlook.com>
2023-10-31 14:11:43 -0300

    updates
    new commits

---
+++ loop.js
@@ -0,0 +1,8 @@
+for (let a=0;a<5;a++){ //o loop for inicializa com a avriavel de controle igual a 0 e conta até  a varaivel ser menor que 5 e vai sendo icrementado +1
+    console.log(a)
+}
+
+let arrey=[1,2,3,4,5,6,7,8,9]; // esse loop percorre uma lista desde o index 0 até o final da lista,
+for(let i=arrey[0]; i<=arrey.length;i++){
+    console.log(i)
+}
