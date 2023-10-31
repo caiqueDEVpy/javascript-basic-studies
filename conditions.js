dac6a0a7a33280fa1f0b3ae2480b203e5f0bcb60
Caique Menezes <caiquemenezes1@outlook.com>
2023-10-31 14:00:08 -0300

    updates
    new commits

---
+++ conditions.js
@@ -0,0 +1,11 @@
+const peso= parseInt(50);
+
+if (peso>parseInt(50)){
+    console.log("Você está acima do peso");
+}
+if (peso==parseInt(50)){
+    console.log("você está dentro do peso para seu nivel");
+} 
+else {
+    console.log("Você está abaixo do peso");
+}
---
+++ hello_world.js
@@ -0,0 +1,3 @@
+const hello_world="hello world!" // cria uma variavel constante para receber uma string
+
+console.log(hello_world) // função para imprimir um dado como paramentro no terminal.
